3b502386eb7d32d302a9f7bff9a6b776d4703366
TPereira324 <tahawurpereira1@gmail.com>
2025-10-16 19:05:45 +0100

    some changes

--- Android/app/src/main/java/pt/iade/ei/bestumbrella1/MainActivity.kt
+++ Android/app/src/main/java/pt/iade/ei/bestumbrella1/MainActivity.kt
@@ -9,11 +9,8 @@ import androidx.compose.ui.Modifier
 import androidx.room.Room
 import pt.iade.ei.bestumbrella1.data.AppDatabase
 import pt.iade.ei.bestumbrella1.data.UserRepository
+import pt.iade.ei.bestumbrella1.navigation.MainNavigation
 import pt.iade.ei.bestumbrella1.ui.theme.BestUmbrella1Theme
-import pt.iade.ei.bestumbrella1.views.LoginScreen
-import pt.iade.ei.bestumbrella1.views.RegisterScreen
-
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,17 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen(
-                        userRepository = userRepository,
-                        onLoginSuccess = { /* Navegar para MapScreen */ },
-                        onRegisterClick = {
-                            RegisterScreen(
-                                userRepository = userRepository,
-                                onRegisterSuccess = { /* Navegar para MapScreen */ },
-                                onLoginClick = { /* Voltar para Login */ }
-                            )
-                        }
-                    )
+                    MainNavigation(userRepository = userRepository)
                 }
             }
         }
