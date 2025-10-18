6700335ad70b885f50bd171a6221dd88e04e5643
TPereira324 <tahawurpereira1@gmail.com>
2025-10-18 17:04:55 +0100

    some changes

--- Android/app/src/main/java/pt/iade/ei/bestumbrella1/views/HistoryScreen.kt
+++ Android/app/src/main/java/pt/iade/ei/bestumbrella1/views/HistoryScreen.kt
@@ -12,9 +12,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-
+import androidx.navigation.compose.rememberNavController
 
 data class RentalEntry(
     val date: String,
@@ -163,4 +164,11 @@ fun HistoryScreen(navController: NavController) {
             }
         }
     }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewHistoryScreen() {
+    val navController = rememberNavController()
+    HistoryScreen(navController)
 }
