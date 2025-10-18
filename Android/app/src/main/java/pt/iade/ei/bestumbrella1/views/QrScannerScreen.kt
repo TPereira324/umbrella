cd0936958729c38518d93b65b82ce444a8333d6e
TPereira324 <tahawurpereira1@gmail.com>
2025-10-18 10:43:27 +0100

    some changes

--- Android/app/src/main/java/pt/iade/ei/bestumbrella1/views/QrScannerScreen.kt
+++ Android/app/src/main/java/pt/iade/ei/bestumbrella1/views/QrScannerScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -37,7 +36,7 @@ fun QrScannerScreen(
     onCodeScanned: (String) -> Unit = {}
 ) {
     val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
+    val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner.current
     var startScanner by remember { mutableStateOf(false) }
 
     var hasCameraPermission by remember {
