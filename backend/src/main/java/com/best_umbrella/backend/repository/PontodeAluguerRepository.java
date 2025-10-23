a4f5d6b3c5b50f0ac2a8cb4ab656818fa171d292
TPereira324 <tahawurpereira1@gmail.com>
2025-10-23 14:03:10 +0100

    modify some files and had some new line code

--- backend/src/main/java/com/best_umbrella/backend/repository/PontodeAluguerRepository.java
+++ backend/src/main/java/com/best_umbrella/backend/repository/PontodeAluguerRepository.java
@@ -1,9 +1,8 @@
 package com.best_umbrella.backend.repository;
 
+import com.best_umbrella.backend.model.PontodeAluguer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.best_umbrella.backend.model.PontodeAluguer;
 
 @Repository
 public interface PontodeAluguerRepository extends JpaRepository<PontodeAluguer, Long> {
