afb3ce31aca4d499dba4eabc17cc16105f668118
TPereira324 <tahawurpereira1@gmail.com>
2025-10-28 16:55:59 +0000

    modifying the coordinates

--- Android/app/src/main/java/pt/iade/ei/bestumbrella1/views/MapScreen.kt
+++ Android/app/src/main/java/pt/iade/ei/bestumbrella1/views/MapScreen.kt
@@ -119,7 +119,7 @@ fun MapScreenWithMarkers(navController: NavController) {
                     )
                 )
         ) {
-            // 🗺️ Google Map com marcadores
+            //  Google Map com marcadores
             GoogleMap(
                 modifier = Modifier.fillMaxSize(),
                 cameraPositionState = cameraPositionState
